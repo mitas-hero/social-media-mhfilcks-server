6b7b0824d9ec5f9097066eddb5ff89a5af4a7682
Mh Mitas <mahfuzulmitas@gmail.com>
2024-08-14 15:04:04 +0600

    add mongo u and up directly

--- src/db/db.js
+++ src/db/db.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jt5df8u.mongodb.net/mhflicks?retryWrites=true&w=majority&appName=Cluster0`)
+        await mongoose.connect(`mongodb+srv://mahimbabu:mahimbabu_mhmahim@cluster0.jt5df8u.mongodb.net/mhflicks?retryWrites=true&w=majority&appName=Cluster0`)
         console.log("MongoDB 🍃 connected")
     } catch (error) {
         console.error("MongoDB connection error:", error);
