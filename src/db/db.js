606a55100f1d9f66dbb98fe8c5aa508b4ea32f0e
Mh Mitas <mahfuzulmitas@gmail.com>
2024-08-14 14:52:54 +0600

    add atlas uri

--- src/db/db.js
+++ src/db/db.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config()
 
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`mongodb://localhost:27017/mhflicks`)
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jt5df8u.mongodb.net/mhflicks?retryWrites=true&w=majority&appName=Cluster0`)
         console.log("MongoDB 🍃 connected")
     } catch (error) {
         console.error("MongoDB connection error:", error);
