ed4d21f17ec8641dda7ca795aa5469592c27242b
Mh Mitas <mahfuzulmitas@gmail.com>
2024-08-14 15:33:02 +0600

    add new domain

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 export const app = express()
 
 app.use(cors({
-    origin: ['*', "http://localhost:5173", "http://localhost:51734"], credentials: true
+    origin: ["http://localhost:5173", "http://localhost:51734", "https://mhflicks-socialmedia.netlify.app"], credentials: true
 }));
 app.use(express.json({ limit: '16kb' }));
 app.use(express.static('public'))
